1b8dc816e330507917b5082abc460561cf68b0d5
anand <anandvipul01@gmail.com>
2022-09-18 14:56:04 +0530

    Array Methods

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -1,27 +1,29 @@
-let fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 // Add a new element ('Papaya') into fruits array at end
-
+fruits.push("Papaya");
 // Remove the last element from fruits array
-
+fruits.pop();
 // Log the current length of fruits array
-
+console.log(fruits.length);
 // Remove the first element from fruits and log the value of fruits (use delete arr[0])
-
+delete fruits[0];
 // Log the element on index 0 and 1
-
+console.log(fruits[0], fruits[1]);
 // If the element at 0 index is undefined and index 1 is 'Banana' shift all the values to one lower index (use shift method)
-
+fruits[0] == undefined || fruits[1] == "Banana" ? fruits.shift() : fruits;
 // Add a new element to the start ('Guava') and shift the index of all other to one higher value
-
+fruits.unshift("Guava");
 // Add a new element to the start ('Dragon Fruit') and shift the index of all other to one higher value
-
+fruits.unshift("Dragon Fruit");
 // Log true or false based on the value at index 0 and 1 is 'Dragon Fruit' and 'Guava' or not
-
+fruits[0] == "Dragon Fruit" && fruits[1] == "Guava"
+  ? console.log(true)
+  : console.log(false);
 // Update the value of index 1 to `Pears`
-
+fruits[1] = "Pears";
 // Add the 'Kiwi' and 'Lemon' to the index 1 and 2 and shift the other element to next index (use splice to add element)
-
+fruits.splice(1, 0, "Kiwi", "Lemon");
 // Remove (slice) all the element from index 5
 
 // Create another array named moreFruits with values ['Berries', 'Melons']
